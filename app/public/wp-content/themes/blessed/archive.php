e1b6972092e5253204ede6584b79bfe541111936
inamathi <loottura@gmail.com>
2026-01-21 18:20:24 +0900

    test

--- app/public/wp-content/themes/blessed/archive.php
+++ app/public/wp-content/themes/blessed/archive.php
@@ -4,7 +4,7 @@
 <?php // 現在使用しているテンプレートファイルを表示
   if (is_user_logged_in()){ // ログイン中なら以下を表示
     global $template; // テンプレートファイルのパスを取得
-    $temp_name = basename($template); // パスの最後の名前（ファイル名）を取得
+    $temp_name = basename($template); // パスの最後の名前（ファイル名）取得
     echo '現在使用しているテンプレートファイル：'.$temp_name; // ファイル名の表示
   }
 ?>
